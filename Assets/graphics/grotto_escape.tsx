1fd3c1b6d88eb4feb790037a6a105b6f28d9f72d
Fredric Dorothy <fredricdorothy@gmail.com>
2019-10-21 20:38:39 -0500

    added basic project structure for a demo

---
+++ Assets/graphics/grotto_escape.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="grotto_escape" tilewidth="16" tileheight="16" tilecount="40" columns="8">
+ <image source="tiles.png" width="128" height="80"/>
+ <terraintypes>
+  <terrain name="ground" tile="34"/>
+  <terrain name="lava" tile="12"/>
+  <terrain name="black" tile="10"/>
+  <terrain name="rocks" tile="34"/>
+  <terrain name="bigrocks" tile="34"/>
+ </terraintypes>
+ <tile id="0" terrain=",0,,"/>
+ <tile id="1" terrain="0,0,,"/>
+ <tile id="2" terrain="0,0,,"/>
+ <tile id="3" terrain="0,,,"/>
+ <tile id="4" terrain=",,1,1"/>
+ <tile id="5" terrain=",,1,1"/>
+ <tile id="9" terrain="2,2,2,2"/>
+ <tile id="10" terrain="2,2,2,2"/>
+ <tile id="11" terrain="2,2,2,2"/>
+ <tile id="12" terrain="1,1,1,1"/>
+ <tile id="13" terrain="3,3,3,3"/>
+ <tile id="14" terrain="4,4,4,4"/>
+ <tile id="15" terrain="4,4,4,4"/>
+ <tile id="18" terrain="2,2,2,2"/>
+ <tile id="20" terrain="3,3,3,3"/>
+ <tile id="21" terrain="3,3,3,3"/>
+ <tile id="22" terrain="4,4,4,4"/>
+ <tile id="23" terrain="4,4,4,4"/>
+ <tile id="24" terrain=",,,0"/>
+ <tile id="25" terrain=",,0,0"/>
+ <tile id="26" terrain=",,0,0"/>
+ <tile id="27" terrain=",,0,"/>
+ <tile id="32" terrain=",0,,0"/>
+ <tile id="33" terrain="0,0,0,0"/>
+ <tile id="34" terrain="0,0,0,0"/>
+ <tile id="35" terrain="0,,0,"/>
+</tileset>
