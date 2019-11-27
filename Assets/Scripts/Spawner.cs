ddca2b8d869e00b98d8790b4cc1ed7fdbad90e17
Fredric Dorothy <fredricdorothy@gmail.com>
2019-11-26 21:42:01 -0600

    switched over to use spawners for faster loading

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject CreaturePrefab;
+    public int spawnCount = 1;
+    public float spawnDelay = 1.0f;
+
+    public void Spawn()
+    {
+        if (spawnCount == 0) {
+            Destroy(this.gameObject);
+            return;
+        }
+        if (spawnCount > 0)
+            spawnCount--;
+        Debug.Log("got this far");
+        GameObject c = Instantiate(CreaturePrefab) as GameObject;
+        c.transform.position = this.transform.position;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.tag == "Player") {
+            Debug.Log("spawning");
+            Spawn();
+            Debug.Log("done");
+        }
+    }
+}
