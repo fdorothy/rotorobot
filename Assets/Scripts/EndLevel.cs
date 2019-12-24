2d306e262ed0ffbc6c72fee2107050c1066a4df4
Fredric Dorothy <fredric.dorothy@vipaar.com>
2019-12-24 09:55:15 -0500

    Laid out text and new levels

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class EndLevel : MonoBehaviour
+{
+    Flowchart chart;
+
+    public void Start() {
+        chart = GameObject.FindObjectOfType<Flowchart>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2d) {
+        if (collider2d.tag == "Player") {
+            chart.ExecuteBlock("LevelEnd");
+        }
+    }
+}
---
+++ Assets/Scripts/StarSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSystem : MonoBehaviour
+{
+    ParticleSystem system;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        system = GetComponent<ParticleSystem>();
+        Slow();
+    }
+
+    public void Fast()
+    {
+        var main = system.main;
+        main.simulationSpeed = 3.0f;
+    }
+
+    public void Slow()
+    {
+        var main = system.main;
+        main.simulationSpeed = 0.1f;
+    }
+
+    public void Stop()
+    {
+        var main = system.main;
+        main.simulationSpeed = 0.0f;
+    }
+}
