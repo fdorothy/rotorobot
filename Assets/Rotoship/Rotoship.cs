6498f16b95c27e23070cc93470389dc1a10ee906
Fredric Dorothy <fredric.dorothy@vipaar.com>
2019-12-24 12:38:03 -0500

    better intro

---
+++ Assets/Rotoship/Rotoship.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotoship : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Warp()
+    {
+        anim.SetBool("warp", true);
+    }
+
+    public void Idle()
+    {
+        anim.SetBool("warp", false);
+    }
+}
